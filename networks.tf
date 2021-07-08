a5fccff3c39263985145051de853591bcefc184e
Alejandro Menocal <alejandromenocal@gmail.com>
2021-07-08 17:07:28 -0500

    add VPC Peering and RTs

--- networks.tf
+++ networks.tf
@@ -65,16 +65,72 @@ resource "aws_subnet" "subnet_1_oregon" {
 
 #Create VPC Peering Connection from us-east-1
 resource "aws_vpc_peering_connection" "useast1-uswest2" {
-    provider = aws.region-master
-    vpc_id = aws_vpc.vpc_master.id
-    peer_vpc_id = aws_vpc.vpc_master_oregon.id
-    peer_region = var.region-worker
-  
+  provider    = aws.region-master
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_vpc_id = aws_vpc.vpc_master_oregon.id
+  peer_region = var.region-worker
+
 }
 
 #Create an Accepter VPC Peering request in us-west-2 from us-east-1
 resource "aws_vpc_peering_connection_accepter" "accept_peering" {
-  provider = aws.region-worker
+  provider                  = aws.region-worker
   vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
-  auto_accept = true
+  auto_accept               = true
+}
+
+#Create route table in us-east-1
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+  }
+  route {
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+    cidr_block                = "192.168.1.0/24"
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    "Name" = "Master-Region-RT"
+  }
+
+}
+
+#Overwrites Main RT(default) from us-east-1 VPC
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master.id
+  route_table_id = aws_route_table.internet_route.id
+}
+
+#Create route table in us-west-2
+resource "aws_route_table" "internet_route_oregon" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_oregon.id
+  route {
+    gateway_id = aws_internet_gateway.igw-oregon.id
+    cidr_block = "0.0.0.0/0"
+  }
+  route {
+    vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
+    cidr_block                = "10.0.1.0/24"
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    "Name" = "Worker-Region-RT"
+  }
+
+}
+
+#Overwrites Main RT(default) from us-west-2 VPC
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-worker
+  vpc_id         = aws_vpc.vpc_master_oregon.id
+  route_table_id = aws_route_table.internet_route_oregon.id
 }
