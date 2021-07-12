ddb449273ae1f23bcab112c83b9c487060fa9357
Alejandro Menocal <alejandromenocal@gmail.com>
2021-07-12 11:40:52 -0500

    added tf providers to ec2

--- instances.tf
+++ instances.tf
@@ -40,6 +40,16 @@ resource "aws_instance" "jenkins-master" {
   }
 
   depends_on = [aws_main_route_table_association.set-master-default-rt-assoc]
+
+  provisioner "local-exec" {
+
+    command = <<EOF
+    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
+    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+    EOF
+
+  }
+  
 }
 
 resource "aws_instance" "jenkins-worker-oregon" {
@@ -57,4 +67,15 @@ resource "aws_instance" "jenkins-worker-oregon" {
   }
 
   depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc, aws_instance.jenkins-master]
+
+  provisioner "local-exec" {
+
+    command = <<EOF
+    aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
+    ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
+    EOF
+
+  }
+
+
 }
